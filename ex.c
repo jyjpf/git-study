d8669b6ab383495a782eaa4da05dcc82b4ba305f
jyjpf <piano908@naver.com>
2020-07-02 15:45:12 +0900

    new c exam file [add]

---
+++ ex.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int sum( int v1, int v2)
+
+int madin(int args, char*, argv[]) {
+	
+
+	int result;
+	result = sum (10, 20);
+	printf("result = %d", result);
+
+	return 0;
+}
+int sum( int v1, int v2){
+	return v1+ v2;
+}
